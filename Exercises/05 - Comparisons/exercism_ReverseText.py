c29793b322f45ae12ad14c32acda459d5046d127
John Enzo Lagarto <johnenzo.23@gmail.com>
2024-01-28 07:09:32 +0800

    Add Comparison Exercise | Reverse Text

--- Exercises/05 - Comparisons/exercism_ReverseText.py
+++ Exercises/05 - Comparisons/exercism_ReverseText.py
@@ -1,5 +1,5 @@
 def reverse(text):
-    """Reverse a string from left to right to right to left
+    """Reverse a text from left to right to right to left
     
     :param text: str - text to be reversed
     :return: str - reversed text"""
